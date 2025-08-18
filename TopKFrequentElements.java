52ad184ba0f97a76786cb186f37bbe85f5592b1c
Javi Loureiro <78276672+javiloureiro@users.noreply.github.com>
2025-08-18 20:31:49 +0200

    Create TopKFrequentElements.java

---
+++ TopKFrequentElements.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            map.merge(nums[i], 1, Integer::sum);
+        }
+
+        return map.entrySet()
+            .stream()
+            .sorted((a,b) -> b.getValue().compareTo(a.getValue()))
+            .limit(k)
+            .mapToInt(Map.Entry::getKey)
+            .toArray();
+    }
+}
